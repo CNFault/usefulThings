56259a9a7a8f9b1d752d2bfb4faf83b2712117b2
Alex Markov <49829241+CNFault@users.noreply.github.com>
2020-06-20 14:12:10 +0700

    Add files via upload

--- src/ArrayColumn.js
+++ src/ArrayColumn.js
@@ -1,9 +1,9 @@
 class ArrayColumn { //описывает поведение колонки. Через него создаем экземляры колонок
-	constructor(order, index) { //функция инициализации экземпляра, вызывается при создании нового экземпляра класса 
-		this.index = index
+	constructor(order, number) { //функция инициализации экземпляра, вызывается при создании нового экземпляра класса 
+		this.number = number
 
 		const div = document.createElement('div')
-		div.innerHTML = `<div class="vcolumn" style="order: ${order}"><p>${this.index}</p></div>`
+		div.innerHTML = `<div class="vcolumn" style="order: ${order}"><p>${this.number}</p></div>`
 
 		this.view = div.firstChild // осуществляется доступ к дом жлементу который отображает колонку
 	}
--- src/script.js
+++ src/script.js
@@ -1,42 +1,36 @@
 const startButton = document.querySelector('.start');
-const aniSpeed = document.querySelector('.i-13');
-document.querySelector('.arrayLengthConfirm').onclick = f4();
-
-function f4() {
-	arrayLength = document.querySelector('.i-14').value;
-	// location.reload(); // обновить страницу
-	return arrayLength
-} //не обновляется число столбцов после нажатия кнопки!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
+const aniSpeed = document.querySelector('.aniSpeed');
 const MAX_HEIGHT = 500; //макс высота самого большого столбца
 const MIN_HEIGHT = 50; //мин высота самого маленького столбца
-
-
 const generateScaleY = (min, max) => n => MIN_HEIGHT + (MAX_HEIGHT - MIN_HEIGHT) * ((n - min) / (max - min)); // нужно масштабировать колонки в каком-то пределе. 
 //его задаем через min max height
 const appElement = document.querySelector('.app');
-
 const array = [];
 
-let aq = +prompt('Введите длину массива');
+let arrayLength = +prompt('Введите длину массива от 0 до 80');
 
-for (let i = 0; i < aq; i++) {
-	const index = Math.floor(Math.random() * 100)
-	const number = new ArrayColumn(i, index) //arrCol
 
-	array.push(number)
+while (arrayLength < 0 || arrayLength > 80) {
+	alert('Пожалуйста, введите число в указанном диапазоне')
+	arrayLength = +prompt('Введите длину массива от 0 до 80');
 }
 
-document.querySelector('.initialData').innerHTML = array.map(x => x.index).join(', '); //не передает массив!!!!!!!!!!!!!!!!!!!!!!!!!
+for (let i = 0; i < arrayLength; i++) {
+	const number = Math.floor(Math.random() * 100)
+	const arrCol = new ArrayColumn(i, number) //arrCol
+
+	array.push(arrCol)
+}
+
+document.querySelector('.initialData').innerHTML = array.map(x => x.number).join(', '); //не передает массив!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 const scaleY = generateScaleY(
-	Math.min(...array.map(x => x.index)), // ищем минимум из всего массива (спред для того чтобы в math.min передать массив)
-	Math.max(...array.map(x => x.index)) // ищем максимум из всего массива
+	Math.min(...array.map(x => x.number)), // ищем минимум из всего массива (спред для того чтобы в math.min передать массив)
+	Math.max(...array.map(x => x.number)) // ищем максимум из всего массива
 )
 
-array.forEach(number => number.view.style.height = `${scaleY(number.index)}px`); //для каждого элемента задаем высоту колонки
+array.forEach(arrCol => arrCol.view.style.height = `${scaleY(arrCol.number)}px`); //для каждого элемента задаем высоту колонки
 
 appElement.append(...array.map(x => x.view)); //помещаем каждый столбец в дом!!!!!!!!!!!!!!
 
@@ -53,7 +47,7 @@ startButton.addEventListener('click', async () => {
 
 			await sleep((2000 - aniSpeed.value) / 2);
 
-			if (columnLeft.index > columnRight.index) { // сравниваем i & i+1
+			if (columnLeft.number > columnRight.number) { // сравниваем i & i+1
 				columnLeft.view.classList.add('changed'); // при true элементам присваиваем класс changed
 				columnRight.view.classList.add('changed'); // при true элементам присваиваем класс changed
 
@@ -70,7 +64,7 @@ startButton.addEventListener('click', async () => {
 			columnRight.view.classList.remove('selected', 'changed'); // удаляем ранее присвоенные классы
 
 			array.sort((a, b) => a.order - b.order); //одна копия сам массив, другая в дом дереве. Синхронизация видимой и программной части
-			document.querySelector('.finaleData').innerHTML = array.map(x => x.index).join(', ');
+			document.querySelector('.finaleData').innerHTML = array.map(x => x.number).join(', ');
 
 		}
 
